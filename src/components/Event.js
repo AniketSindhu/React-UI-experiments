339c0bf583c8209d124aebfce52f70b65aaee6e5
Aniket Sindhu <sindhuaniket@gmail.com>
2021-05-12 08:44:15 +0530

    init

---
+++ src/components/Event.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import'../componentsCss/event.css'
+
+function Event() {
+    return (
+        <div className="Container">
+            <h3 className="Title">Aniket Sindhu</h3>
+        </div>
+    )
+}
+
+export default Event
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/Poppins-Medium.ttf'
+import './fonts/Poppins-Regular.ttf'
+import './fonts/Poppins-Bold.ttf'
 
 ReactDOM.render(
   <React.StrictMode>
